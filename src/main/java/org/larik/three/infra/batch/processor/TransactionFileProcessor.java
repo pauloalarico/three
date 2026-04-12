4289dc739d53871eec8ca95788feba0042928ffd
pauloalarico <paulocraft20@gmail.com>
2026-04-11 23:53:21 -0300

    feature: create processor

---
+++ src/main/java/org/larik/three/infra/batch/processor/TransactionFileProcessor.java
@@ -0,0 +1,18 @@
+package org.larik.three.infra.batch.processor;
+
+import org.jspecify.annotations.Nullable;
+import org.larik.three.domain.model.Transaction;
+import org.springframework.batch.infrastructure.item.ItemProcessor;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+public class TransactionFileProcessor implements ItemProcessor<Transaction, Transaction> {
+
+    @Override
+    public @Nullable Transaction process(Transaction item) throws Exception {
+        item.setTransactionId(UUID.randomUUID().toString());
+        return item;
+    }
+}
