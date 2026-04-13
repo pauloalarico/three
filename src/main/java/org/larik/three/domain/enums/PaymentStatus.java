89906dcb0227e0c8e3170a029eda9f39a2451b77
pauloalarico <paulocraft20@gmail.com>
2026-04-12 23:48:55 -0300

    feature: create methods to convert the string different languages status to enum

--- src/main/java/org/larik/three/domain/enums/PaymentStatus.java
+++ src/main/java/org/larik/three/domain/enums/PaymentStatus.java
@@ -1,7 +1,34 @@
 package org.larik.three.domain.enums;
 
+import lombok.*;
+
+import java.util.HashMap;
+
+@RequiredArgsConstructor
 public enum PaymentStatus {
-    PAID,
-    PENDENT,
-    CANCELED
+    PAID("APROVADA", "PAID"),
+    PENDENT("PENDENTE", "PENDENT"),
+    CANCELED("RECUSADA", "CANCELED");
+
+    private final String localName;
+
+    private final String globalName;
+
+    private final static HashMap<String, PaymentStatus> BY_LOCAL_NAME = new HashMap<>();
+
+    static {
+        for (PaymentStatus e : values()) {
+            BY_LOCAL_NAME.put(e.localName, e);
+        }
+    }
+
+    public static PaymentStatus getByLocalName(String status) {
+        PaymentStatus constant = BY_LOCAL_NAME.get(status);
+        if(constant == null) {
+            throw new IllegalArgumentException("Enum not found");
+        }
+
+        return constant;
+    }
+
 }
