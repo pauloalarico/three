d8ddd34938fbd03486e7f9eefac6752143b2e955
pauloalarico <paulocraft20@gmail.com>
2026-04-13 00:25:33 -0300

    chore: add env and lib configuration's

--- src/main/java/org/larik/three/infra/batch/writer/TransactionWriterConfig.java
+++ src/main/java/org/larik/three/infra/batch/writer/TransactionWriterConfig.java
@@ -1,9 +1,10 @@
-package org.larik.three.infra.batch;
+package org.larik.three.infra.batch.writer;
 
 import lombok.RequiredArgsConstructor;
 import org.larik.three.domain.model.Transaction;
 import org.springframework.batch.infrastructure.item.ItemWriter;
 import org.springframework.batch.infrastructure.item.data.MongoItemWriter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -12,6 +13,7 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 @RequiredArgsConstructor
 public class TransactionWriterConfig {
 
+    @Value("${app.mongoCollection}")
     private String collection;
 
     private final MongoTemplate mongoTemplate;
@@ -19,7 +21,7 @@ public class TransactionWriterConfig {
     @Bean
     public ItemWriter<Transaction> writer() {
         MongoItemWriter<Transaction> writer = new MongoItemWriter<>(mongoTemplate);
-        writer.setCollection("writer-teste");
+        writer.setCollection(collection);
         return writer;
     }
 
