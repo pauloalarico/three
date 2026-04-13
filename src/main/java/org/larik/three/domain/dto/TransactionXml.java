2aa5a8c2310f7d1d75211e96a09f6f7988caa804
pauloalarico <paulocraft20@gmail.com>
2026-04-12 23:48:07 -0300

    feature: create dto for xml parsing to model

---
+++ src/main/java/org/larik/three/domain/dto/TransactionXml.java
@@ -0,0 +1,34 @@
+    package org.larik.three.domain.dto;
+
+    import jakarta.xml.bind.annotation.XmlAccessType;
+    import jakarta.xml.bind.annotation.XmlAccessorType;
+    import jakarta.xml.bind.annotation.XmlElement;
+    import jakarta.xml.bind.annotation.XmlRootElement;
+    import lombok.AllArgsConstructor;
+    import lombok.Data;
+    import lombok.NoArgsConstructor;
+
+    import java.math.BigDecimal;
+
+    @XmlRootElement(name = "Transacao")
+    @XmlAccessorType(XmlAccessType.FIELD)
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public class TransactionXml {
+
+        @XmlElement(name = "ClienteCodigo")
+        private Long clientId;
+
+        @XmlElement(name = "ClienteNome")
+        private String clientName;
+
+        @XmlElement(name = "Valor")
+        private BigDecimal paymentValue;
+
+        @XmlElement(name = "Data")
+        private String paymentDate;
+
+        @XmlElement(name = "Situacao")
+        String paymentStatus;
+    }
