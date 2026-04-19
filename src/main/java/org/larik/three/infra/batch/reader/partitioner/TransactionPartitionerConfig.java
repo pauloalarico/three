0751c059e8ff668d32a2ca10b46f036c0b7c709b
pauloalarico <paulocraft20@gmail.com>
2026-04-18 23:46:36 -0300

    feat: add partitioner as step to run with multi thread

--- src/main/java/org/larik/three/infra/batch/reader/partitioner/TransactionPartitionerConfig.java
+++ src/main/java/org/larik/three/infra/batch/reader/partitioner/TransactionPartitionerConfig.java
@@ -1,19 +1,26 @@
 package org.larik.three.infra.batch.reader.partitioner;
 
+import org.springframework.batch.core.partition.support.MultiResourcePartitioner;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.Step;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+@Configuration
 public class TransactionPartitionerConfig {
 
-//    @Bean
-//    public Step partioner(JobRepository jobRepository) {
-//        return new StepBuilder(jobRepository, "thread-partitioner")
-//                .
-//    }
+    @Bean
+    public Step partitioner(JobRepository jobRepository, Step step, TaskExecutor taskExecutor) {
+        return new StepBuilder("thread-partitioner", jobRepository)
+                .partitioner("partition-reader", new MultiResourcePartitioner())
+                .step(step)
+                .taskExecutor(taskExecutor)
+                .gridSize(4)
+                .build();
+    }
 
 
     @Bean
