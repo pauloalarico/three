90323717a03e4ed16457f5adbbc47bbd3a11b1ba
pauloalarico <paulocraft20@gmail.com>
2026-04-26 23:52:58 -0300

    feat: implement the processor of financial step

---
+++ src/main/java/org/larik/three/infra/batch/processor/financial/FinancialPositionCalculator.java
@@ -0,0 +1,51 @@
+package org.larik.three.infra.batch.processor.financial;
+
+import org.jspecify.annotations.Nullable;
+import org.larik.three.domain.dto.comparison.ComparisonTransactionResult;
+import org.larik.three.domain.model.*;
+import org.springframework.batch.infrastructure.item.ItemProcessor;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+@Component
+public class FinancialPositionCalculator implements ItemProcessor<ComparisonTransactionResult, ProcessedTransaction>  {
+
+    private static final BigDecimal PERCENT = new BigDecimal("0.01");
+
+    private static final int[] MONTHS = {1,2,3};
+
+    @Override
+    public @Nullable ProcessedTransaction process(ComparisonTransactionResult item) throws Exception {
+        BigDecimal diff = item.expectedTransaction().getPayment().getValue().subtract(item.rawTransaction().getPayment().getValue());
+        Transaction raw = item.rawTransaction();
+        Transaction expected = item.expectedTransaction();
+
+        return ProcessedTransaction.builder().
+                client(Client.builder()
+                        .clientId(raw.getClient().getClientId())
+                        .name(raw.getClient().getName())
+                        .build())
+                .status((item.status()))
+                .remittance(new Remittance(
+                        new ExpectedRemittance(raw.getPayment().getDate(), raw.getPayment().getValue()),
+                        new RawRemittance(expected.getPayment().getDate(), expected.getPayment().getValue()))
+                )
+                .balance(Balance.builder()
+                        .realBalance(raw.getPayment().getValue())
+                        .expectedAmount(expected.getPayment().getValue())
+                        .balanceDifference(diff)
+                        .taxBalanceWithMonth(taxBalanceCalculator(raw.getPayment().getValue(), MONTHS[0]))
+                        .taxBalanceWithTwoMonths((taxBalanceCalculator(raw.getPayment().getValue(), MONTHS[1])))
+                        .taxBalanceWithThreeMonths((taxBalanceCalculator(raw.getPayment().getValue(), MONTHS[2])))
+                        .build())
+                .build();
+    }
+
+    private BigDecimal taxBalanceCalculator(BigDecimal value, int month) {
+        var tax = value.multiply(PERCENT).multiply(BigDecimal.valueOf(month));
+        return value.add(tax).setScale(2, RoundingMode.HALF_UP);
+    }
+
+}
