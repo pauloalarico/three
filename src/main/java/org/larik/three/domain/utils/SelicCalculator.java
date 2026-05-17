245bb272b16237a7d8be4262d2e924e42ef67090
pauloalarico <paulocraft20@gmail.com>
2026-05-17 01:58:17 -0300

    refactor: change the stereotype of class and add a fallback value in case of bcb services turning down

--- src/main/java/org/larik/three/domain/utils/SelicCalculator.java
+++ src/main/java/org/larik/three/domain/utils/SelicCalculator.java
@@ -1,10 +1,10 @@
 package org.larik.three.domain.utils;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 import tools.jackson.databind.ObjectMapper;
 
-import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.net.URI;
@@ -12,21 +12,34 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
-@Component
+@Slf4j
+@Configuration
 public class SelicCalculator {
 
     private static final String SELIC_CONSULT = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.4390/dados/ultimos/1?formato=json";
+    private static final String DEFAULT_VALUE = "0.54";
 
     @Bean
-    public BigDecimal selicTaxCalculator() throws IOException, InterruptedException {
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(SELIC_CONSULT))
-                .GET().build();
+    public BigDecimal selicTaxCalculator() {
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(SELIC_CONSULT))
+                    .GET().build();
 
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            return parse(response);
+
+        } catch (Exception e) {
+            log.error("BCB API unavailable. Using default value. At: {}", e.getMessage());
+            return new BigDecimal(DEFAULT_VALUE);
+        }
+
+    }
+
+    private BigDecimal parse(HttpResponse<String> response) {
         ObjectMapper objectMapper = new ObjectMapper();
-        SelicValue [] selics = objectMapper.readValue(response.body(), SelicValue[].class);
+        SelicValue[] selics = objectMapper.readValue(response.body(), SelicValue[].class);
         var selic = selics[0];
         return new BigDecimal(selic.value()).divide(new BigDecimal("100"), RoundingMode.HALF_UP);
     }
